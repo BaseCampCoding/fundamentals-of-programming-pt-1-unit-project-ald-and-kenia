dae11d737e70e5b0012a3b37e723b551a0307c7f
Al Dees <adees21@basecampcodingacademy.org>
2020-10-05 16:23:54 -0500

    50

--- project.py
+++ project.py
@@ -28,7 +28,7 @@ def level_two():
 
     """ 
     guess = int(input("enter a number from 1 to 10: "))
-    num = random.randint(1,20)
+    num = random.randint(1,50)
     lives = 4 
     while num in range(1,50):
         if guess == num:
