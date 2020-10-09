68823d1a724ac679d632f56b6ac5c86ab7dd1aa2
Kenia Sandoval <ksandoval-lopez21@basecampcodingacademy.org>
2020-10-09 08:38:35 -0500

    added 5 to level 5

--- project.py
+++ project.py
@@ -362,7 +362,21 @@ def level_five():
                 else:
                     print("please add valid input")
                     reply = input("Do you want a hint? Y/N:").upper()
-
+        elif num5 % 5 == 0:
+            lives -= 1
+            print("Incorrect")
+            print("lives:" +str(lives))
+            reply = input("Do you want a hint? Y/N:")
+            if reply == 'Y':
+                print("The correct number is a divisible by 5! Try guessing lower! \n ")
+            while True:
+                guess = input("Enter a number from 1 to 1000: ")
+                if guess.isdigit():
+                    guess= int(guess)
+                    break
+                else:
+                    print("Please select a value of 0 or greater")
+                    
             while True:
                 guess = input("Enter a number from 1 to 1000: ")
                 if guess.isdigit():
